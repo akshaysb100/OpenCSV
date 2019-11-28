5fdd71e10123016456e856d75e0ae224e8db7eca
akshaysb100 <akshaybavalekar100@gmail.com>
2019-11-28 19:06:14 +0530

    Add:Read CSV file using pojo class

--- src/main/java/com/opencsv/com/openCSVReadAndParseToBean.java
+++ src/main/java/com/opencsv/com/openCSVReadAndParseToBean.java
@@ -1,6 +1,42 @@
 package com.opencsv.com;
 
+import com.opencsv.CSVReader;
+import com.opencsv.bean.CsvToBean;
+import com.opencsv.bean.CsvToBeanBuilder;
+
+import java.io.IOException;
+import java.io.Reader;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Iterator;
+import java.util.List;
+
 public class openCSVReadAndParseToBean {
 
+    private static final String SAMPLE_CSV_FILE_PATH = "/home/user/IdeaProjects/OpenCSVFile/src/main/java/user.csv";
 
+    public static void main(String[] args) {
+
+        try {
+            Reader reader = Files.newBufferedReader(Paths.get(SAMPLE_CSV_FILE_PATH));
+
+            CsvToBean<CSVUser> cavToBean = new CsvToBeanBuilder(reader)
+                    .withType(CSVUser.class)
+                    .withIgnoreLeadingWhiteSpace(true)
+                    .build();
+            Iterator<CSVUser> csvUserIterator = cavToBean.iterator();
+            while (csvUserIterator.hasNext()) {
+
+                CSVUser csvUser = csvUserIterator.next();
+                System.out.println("Name : " + csvUser.getName());
+                System.out.println("Email : " + csvUser.getEmail());
+                System.out.println("Phone : " + csvUser.getPhoneNo());
+                System.out.println("Country : " + csvUser.getCountry());
+                System.out.println("*****************************");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
 }
